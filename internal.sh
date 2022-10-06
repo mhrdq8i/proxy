41192d83931696fee8b279a49d06d1772be06084
Mehrdad <mehrdad.drpc@gmail.com>
2022-10-06 12:32:09 +0000

    Add miss command

--- internal.sh
+++ internal.sh
@@ -8,5 +8,5 @@ sysctl -w net.ipv4.ip_forward=1
 iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
 iptables -t nat -A PREROUTING -d $int_server/32 -p tcp -m tcp --dport 80  -j DNAT --to-destination $ext_server:80
 iptables -t nat -A PREROUTING -d $int_server/32 -p tcp -m tcp --dport 443 -j DNAT --to-destination $ext_server:443
-iptables -A POSTROUTING -j MASQUERADE
+iptables -t nat -A POSTROUTING -j MASQUERADE
 iptables -A FORWARD -j ACCEPT
