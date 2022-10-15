574d01a9f386ee805aa77b538470a535137b4459
Mehrdad <40826835+mehrdad-drpc@users.noreply.github.com>
2022-10-15 20:59:59 +0330

    Remove port 443

--- internal.sh
+++ internal.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-read -p "Enter -Internal- server address: " int_server
-read -p "Enter -External- server address: " ext_server
+read -p "Enter -Internal- server address: " INT_SERVER
+read -p "Enter -External- server address: " EXT_SERVER
 
 sysctl -w net.ipv4.ip_forward=1
 
 iptables -t filter -D INPUT -p tcp -m tcp --dport 80 -j ACCEPT 2>/dev/null
-iptables -t nat -D PREROUTING -d $int_server/32 -p tcp -m tcp --dport 80  -j DNAT --to-destination $ext_server:80 2>/dev/null
-iptables -t nat -D PREROUTING -d $int_server/32 -p tcp -m tcp --dport 443 -j DNAT --to-destination $ext_server:443 2>/dev/null
+iptables -t nat -D PREROUTING -d $INT_SERVER/32 -p tcp -m tcp --dport 80  -j DNAT --to-destination $EXT_SERVER:80 2>/dev/null
 iptables -t nat -D POSTROUTING -j MASQUERADE 2>/dev/null
 iptables -t filter -D FORWARD -j ACCEPT 2>/dev/null
 
 iptables -t filter -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
-iptables -t nat -A PREROUTING -d $int_server/32 -p tcp -m tcp --dport 80  -j DNAT --to-destination $ext_server:80
-iptables -t nat -A PREROUTING -d $int_server/32 -p tcp -m tcp --dport 443 -j DNAT --to-destination $ext_server:443
+iptables -t nat -A PREROUTING -d $INT_SERVER/32 -p tcp -m tcp --dport 80  -j DNAT --to-destination $EXT_SERVER:80
 iptables -t nat -A POSTROUTING -j MASQUERADE
 iptables -t filter -A FORWARD -j ACCEPT
