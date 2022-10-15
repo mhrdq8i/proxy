69c645d3a2acce3929f613bdf12787343a36a151
Mehrdad <40826835+mehrdad-drpc@users.noreply.github.com>
2022-10-15 19:44:45 +0330

    Fix IR IP in config

--- external.sh
+++ external.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-read -p "Enter -IR- server address: " ir_server
+read -p "Enter -IR- server address: " IR_SERVER
 
 # Add iptables rules
-iptables -t filter -D INPUT -s $ir_server/32 -p tcp -m tcp --dport 80  -j ACCEPT 2>/dev/null
-iptables -t filter -D INPUT -s $ir_server/32 -p tcp -m tcp --dport 443  -j ACCEPT 2>/dev/null
+iptables -t filter -D INPUT -s $IR_SERVER/32 -p tcp -m tcp --dport 80  -j ACCEPT 2>/dev/null
+iptables -t filter -D INPUT -s $IR_SERVER/32 -p tcp -m tcp --dport 443  -j ACCEPT 2>/dev/null
 
-iptables -t filter -A INPUT -s $ir_server/32 -p tcp -m tcp --dport 80 -j ACCEPT
-iptables -t filter -A INPUT -s $ir_server/32 -p tcp -m tcp --dport 443 -j ACCEPT
+iptables -t filter -A INPUT -s $IR_SERVER/32 -p tcp -m tcp --dport 80 -j ACCEPT
+iptables -t filter -A INPUT -s $IR_SERVER/32 -p tcp -m tcp --dport 443 -j ACCEPT
 
 # Install docker and docker-compose
 apt update -y
@@ -91,7 +91,7 @@ docker-compose up -d
 
 # User output
 printf "SET These to your v2ray client.\n"
-echo "* IP: $IP"
+echo "* IP: $IR_SERVER"
 echo "* Port: $PORT"
 echo "* UUID: $UUID"
 echo "* Alter-ID: $AID"
